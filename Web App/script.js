8e5b2bb8ad02951b67ea05b1f6364bdf87754779
CEOFYEAST <benton.diebold@me.com>
2024-04-29 16:35:58 -0500

    added explicit differentiation between URPS resulting from input vs URPS resulting from output, and accounted for parent of production tree in calculateProduction method.

--- Web App/script.js
+++ Web App/script.js
@@ -1,11 +1,20 @@
 /**
+ * Factorio Production 
+ * Author: Benton Diebold (CEOFYEAST)
  * Current Problems:
  * - CR is un-accounted for
  * - Crafting divisor is just set to one because it's currently impossible to tell which crafter a given recipe is using
- * - the original paren aka. original input is un-accounted for in the output
  * - the UI is completely un-implemented
- * - input and output URPS aren't differentiated between
+ * - data validation / error handling is garbo
+ *
+ * Added:
+ * - added explicit differentiation between URPS resulting from input vs URPS resulting from output.
+ * - accounted for parent of production tree in calculateProduction method.
  */
+
+//- Using a function pointer:
+document.getElementById("button").onclick = getRecipes();
+
 function getRecipes() {
   $.getJSON("https://kevinta893.github.io/factorio-recipes-json/recipes.dictionary.min.json", function(json, err) {
     if (err != "success") {
@@ -13,28 +22,41 @@ function getRecipes() {
       return;
     }
 
-    return json;
+    var recipes = json;
+
+    output = {};
+
+    calculateProduction("tank", 60, recipes, output);
+
+    // runs for every ingredient
+    for (let outputKey in output) {
+      outputVal = output[outputKey];
+      outputURPS = outputVal["Output URPS"];
+
+      console.log(outputKey + " : " + outputURPS);
+    }
   });
 }
 
-function calculateProduction() {
-  let output = {};
+/**
+ * Calculates the required production rate per second (URPS) of all the ingredients required to craft a given recipe as well as the crafter counts required per ingredient.
+ *
+ * @param {string} parentID - The ID of the parent recipe.
+ * @param {number} parentURPS - The Units Required Per Second (URPS) of the parent recipe.
+ * @param {object} recipes - The dictionary containing all recipes.
+ * @param {object} output - The output dictionary to store calculated URPS for each child ingredient.
+ */
+function calculateProduction(parentID, parentURPS, recipes, output) {
+  validateInput(parentID, parentURPS, recipes, output);
 
+  tryAddToOutput(parentID, recipes, output);
 
+  parent = output[parentID];
 
-  calculateChildrenURPS("inserter", 3, recipesDict, output)
+  parent["Input URPS"] += parentURPS;
 
-  // runs for every ingredient
-  for (let outputKey in output) {
-    outputVal = output[outputKey];
-    outputURPS = outputVal["URPS"];
-
-    console.log(outputKey + " : " + outputURPS);
-  }
+  calculateChildrenURPS(parentID, parentURPS, recipes, output);
 }
-
-//- Using a function pointer:
-document.getElementById("button").onclick = getRecipes();
 
 /**
  * Recursively calculates the Units Required Per Second (URPS) of all the ingredients required to produce the given URPS of the given parent.
@@ -43,22 +65,10 @@ document.getElementById("button").onclick = getRecipes();
  * @param {number} parentURPS - The Units Required Per Second (URPS) of the parent recipe.
  * @param {object} recipes - The dictionary containing all recipes.
  * @param {object} output - The output dictionary to store calculated URPS for each child ingredient.
- * @throws {string} Throws an error if any input values are null, if parent URPS is invalid, if parentID is an empty string, or if recipe is not found in recipes.
  * @returns {void} This function does not return a value explicitly, but updates the output dictionary with calculated URPS.
  */
 function calculateChildrenURPS(parentID, parentURPS, recipes, output) {
-  if (parentID == null || parentURPS == null || recipes == null || output == null) {
-    throw "No input values can be null";
-  }
-  if (parentURPS <= 0) {
-    throw "URPS must be greater than 0";
-  }
-  if (parentID == "") {
-    throw "id cannot be empty";
-  }
-  if (!recipes.hasOwnProperty(parentID)) {
-    throw "Recipe with id '" + id + "' not found in recipesDict";
-  } ``
+  validateInput(parentID, parentURPS, recipes, output);
 
   let parent = recipes[parentID];
   let parentType = parent["Type"];
@@ -77,16 +87,14 @@ function calculateChildrenURPS(parentID, parentURPS, recipes, output) {
   for (let ingredientKey in parentIngredients) {
     let ingredientDict = parentIngredients[ingredientKey];
     let childID = ingredientDict["id"];
-    let child = recipes[childID];
-    let childType = child["Type"];
+
+    tryAddToOutput(childID, recipes, output);
+
     let childURPC = ingredientDict["amount"];
     let childURPS = childURPC * parentCRPS;
-
-
-
     let childOutput = output[childID];
 
-    childOutput["URPS"] += childURPS;
+    childOutput["Output URPS"] += childURPS;
 
     calculateChildrenURPS(childID, childURPS, recipes, output);
   }
@@ -98,37 +106,64 @@ function calculateCR() {
 
 /**
  * Tries to add the item/resource associated with the given ID to the output dictionary.
- * @param {dictionary} output - The dictionary to add the fresh item/resource representation to.
+ * 
  * @param {number} toAddID - The ID of the item/resource being added; this is the only information necessary to add the item.
- * @returns {boolean} Whether the operation was succesful.
+ * @param {dictionary} recipes - The dictionary containing all recipes.
+ * @param {dictionary} output - The dictionary to add the fresh item/resource representation to.
+ * @returns {boolean} Whether the addition was succesful.
  */
-function tryAddToOutput(output, toAddID) {
-  if (output == null || toAddID == null) {
+function tryAddToOutput(toAddID, recipes, output) {
+  if (toAddID == null || recipes == null || output == null) {
     throw "No input values can be null";
   }
   if (toAddID == "") {
     throw "id cannot be empty";
   }
+  if (!recipes.hasOwnProperty(toAddID)) {
+    throw "Recipe with id '" + id + "' not found in recipesDict...1";
+  }
 
   // adds ingredient representation to output if it doesn't already exist.
-  if (!output.hasOwnProperty(childID)) {
-    let childOutputVals = {
+  if (!output.hasOwnProperty(toAddID)) {
+    let outputVals = {
       "Input URPS": 0,
-      "URPS": 0,
+      "Output URPS": 0,
       "CR": 0
     };
 
-    if (childType == "Resource" || childType == "Liquid") {
-      childOutputVals = {
-        "Input URPS": 0,
-        "URPS": 0
-      };
+    let toAddType = recipes[toAddID]["Type"];
+    if (toAddType == "Resource" || toAddType == "Liquid") {
+      delete outputVals["CR"];
     }
 
-    output[childID] = childOutputVals;
+    output[toAddID] = outputVals;
 
     return true;
   }
 
   return false;
+}
+
+/**
+ * Capable of validating two commonly used input signature within the program.
+ * 
+ * @param {string} id - The ID of the recipe.
+ * @param {number} urps - The Units Required Per Second (URPS) for the recipe.
+ * @param {object} recipes - The dictionary containing all recipes.
+ * @param {object} output - The output dictionary.
+ * @throws {string} Throws error if any input value is null, URPS is less than or equal to 0, ID is empty, or            * recipe corresponding to given id does not exist.
+ */
+function validateInput(id, urps, recipes, output) {
+  if (id == null || recipes == null || output == null || urps == null) {
+    throw "No input values can be null";
+  }
+  if (urps <= 0) {
+    throw "URPS must be greater than 0.";
+  }
+  if (id == "") {
+    throw "id cannot be empty";
+  }
+  if (!recipes.hasOwnProperty(id)) {
+    throw "Recipe with id '" + id + "' not found in recipesDict...2";
+  }
 }
