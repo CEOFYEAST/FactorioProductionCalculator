7832b4972cbfd7b4f004c252cb9fc8cf3685cb6d
CEOFYEAST <benton.diebold@me.com>
2024-05-21 13:41:42 -0500

    experimenting with promises; they seem promising

--- com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/index.js
+++ com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/index.js
@@ -9,11 +9,45 @@ const { calculateChildrenURPS } = require('./calculators.module.js');
 const { getJSON, writeObj, recipesLoc, testDataLoc } = require('./utility.module.js');
 const { updateProduction, updateProductionURPS, tryAddToOutput, printOutput } = require('./output.module.js');
 const fs = require('fs');
+const fsPromises = require('fs').promises; 
 
 var recipes = getJSON(recipesLoc);
 
 let output = {};
 updateProductionURPS('inserter', 35, false, recipes, output);
+fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+    .then(() => {
+        console.log("File has been written successfully");
+    })
+    .then(() => {
+        output = getJSON(testDataLoc);
+        updateProductionURPS('inserter', 34, true, recipes, output);
+        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+    })
+    .then(() => {
+        console.log("File has been altered successfully");
+        output = getJSON(testDataLoc);
+        updateProductionURPS('inserter', 1, true, recipes, output);
+        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+    })
+    .then(() => {
+        console.log("File has been altered successfully");
+        output = getJSON(testDataLoc);
+        updateProductionURPS('satellite', 5, false, recipes, output);
+        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+    })
+    .then(() => {
+        console.log("File has been altered successfully");
+        output = getJSON(testDataLoc);
+        updateProductionURPS('iron-plate', 20, false, recipes, output);
+        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+    })
+    .catch((err) => {
+        console.error(err);
+        return;
+    });
+
+/** 
 fs.writeFile(testDataLoc, JSON.stringify(output, null, 4), (err) => {
     if (err) {
         console.error(err);
@@ -35,3 +69,4 @@ fs.writeFile(testDataLoc, JSON.stringify(output, null, 4), (err) => {
         });
     }
 });
+*/
