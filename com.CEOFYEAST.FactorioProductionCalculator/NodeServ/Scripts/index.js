24d7a8d23ebae4341ec23d5d39f5da17413d94c5
CEOFYEAST <benton.diebold@me.com>
2024-05-30 09:13:20 -0500

    Began experimenting with database integration in private file

--- com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/index.js
+++ com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/index.js
@@ -10,55 +10,31 @@ const { getJSON, writeObj, recipesLoc, testDataLoc } = require('./utility.module
 const { updateProduction, updateProductionURPS, tryAddToOutput, printOutput } = require('./output.module.js');
 const fs = require('fs');
 const fsPromises = require('fs').promises; 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-var recipes = getJSON(recipesLoc);
+const username = encodeURIComponent("CEOFYEAST");
+const password = encodeURIComponent("AwD1SeD1DrG");
+const uri = "mongodb+srv://CEOFYEAST:AwD1SeD1DrG@yeastcluster.dqbtsia.mongodb.net/?retryWrites=true&w=majority&appName=YeastCluster";
 
-let output = {};
-updateProductionURPS('inserter', 1, false, recipes, output);
-fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
-    .then(() => {
-        console.log("File has been written successfully");
-    })
-    .then(() => {
-        output = getJSON(testDataLoc);
-        updateProductionURPS('long-handed-inserter', 1, false, recipes, output);
-        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
-    })
-    .then(() => {
-        output = getJSON(testDataLoc);
-        updateProductionURPS('automation-science-pack', 1, false, recipes, output);
-        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
-    })
-    .then(() => {
-        output = getJSON(testDataLoc);
-        updateProductionURPS('assembling-machine-1', 0.2, false, recipes, output);
-        return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
-    })
-    .catch((err) => {
-        console.error(err);
-        return;
-    });
-
-/** 
-fs.writeFile(testDataLoc, JSON.stringify(output, null, 4), (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    } else {
-        console.log("File has been written successfully\n");
-
-        output = getJSON(testDataLoc);
-        updateProductionURPS('inserter', 34, true, recipes, output);
-        fs.writeFile(testDataLoc, JSON.stringify(output, null, 4), (err) => {
-            if (err) {
-                console.error(err);
-                return;
-            } else {
-                //let outputThree = getJSON(testDataLoc);
-                //updateProductionURPS('inserter', 35, false, recipes, output);
-                
-            }
-        });
-    }
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
-*/
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
--- com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/utility.module.js
+++ com.CEOFYEAST.FactorioProductionCalculator/NodeServ/Scripts/utility.module.js
@@ -35,3 +35,31 @@ module.exports = {
     testDataLoc
 };
 
+function multipleProductionUpdatesExample(){
+    var recipes = getJSON(recipesLoc);
+    let output = {};
+    updateProductionURPS('inserter', 1, false, recipes, output);
+    fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+        .then(() => {
+            console.log("File has been written successfully");
+        })
+        .then(() => {
+            output = getJSON(testDataLoc);
+            updateProductionURPS('long-handed-inserter', 1, false, recipes, output);
+            return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+        })
+        .then(() => {
+            output = getJSON(testDataLoc);
+            updateProductionURPS('automation-science-pack', 1, false, recipes, output);
+            return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+        })
+        .then(() => {
+            output = getJSON(testDataLoc);
+            updateProductionURPS('assembling-machine-1', 0.2, false, recipes, output);
+            return fsPromises.writeFile(testDataLoc, JSON.stringify(output, null, 4))
+        })
+        .catch((err) => {
+            console.error(err);
+            return;
+        });
+}
