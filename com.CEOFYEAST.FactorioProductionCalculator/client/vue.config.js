f2820eb7d6e17d84c52f35e070434027ba462193
CEOFYEAST <benton.diebold@me.com>
2024-10-30 12:28:15 -0700

    Updated public path

--- com.CEOFYEAST.FactorioProductionCalculator/client/vue.config.js
+++ com.CEOFYEAST.FactorioProductionCalculator/client/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/UserSystemPrototype/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/factorio-production-calculator.com/" : "/",
   transpileDependencies: true
 })
