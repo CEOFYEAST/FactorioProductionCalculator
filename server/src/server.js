57ef2631f351faf24c50fa09f9eff685141c98fb
CEOFYEAST <benton.diebold@me.com>
2024-11-06 13:55:53 -0800

    Added shebang interpreter directive to my server file; this directive ensures that the server file is interpeted properly as a node file

--- server/src/server.js
+++ server/src/server.js
@@ -1,35 +1,4 @@
-// const fastify = require('fastify')({logger: true})
-
-// fastify.register(require('@fastify/cors'), {})
-// fastify.register(require('@fastify/swagger'), {})
-// fastify.register(require('@fastify/swagger-ui'), {
-//     routePrefix: '/docs',
-//     uiConfig: {
-//         docExpansion: 'full',
-//         deepLinking: false
-//     },
-//     uiHooks: {
-//         onRequest: function (request, reply, next) { next() },
-//         preHandler: function (request, reply, next) { next() }
-//     },
-//     staticCSP: true,
-//     transformStaticCSP: (header) => header,
-//     transformSpecification: (swaggerObject, request, reply) => { return swaggerObject },
-//     transformSpecificationClone: true
-// })
-// fastify.register(require('./routes/items.js'))
-
-// const start = async () => {
-//     try {
-//         await fastify.listen(process.env.PORT || 5001)
-//     } catch(error) {
-//         fastify.log.error(error)
-//         process.exit(1)
-//     }
-// }
-
-// start()
-
+#!/usr/bin/env node
 
 'use strict'
 
