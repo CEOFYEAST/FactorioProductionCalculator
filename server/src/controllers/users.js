4e1696cc4c4ef477fa1e39b4ec0422d2823ee297
CEOFYEAST <benton.diebold@me.com>
2024-12-27 23:06:51 -0600

    Mostly completed user POST logic; only need to add clarification in the success response on whether an account was actually created

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -6,18 +6,38 @@ const getUser = (req, reply) => {
 }
 
 const addUser = (req, reply) => {
-    console.log("-------------------------------------------------\n")
-
-    console.log(req.body)
     const { userName, userPassword } = req.body
 
-    const db = app.mongo.client.db("sample_users");
-    const coll = db.collection("users")
-    const doc = { userName: userName, userPassword: userPassword }
-    coll.insertOne(doc);
-    
-    console.log("-------------------------------------------------\n")
-    reply.code(201).send(JSON.stringify(req.body))
+    let db = app.mongo.client.db("sample_users");
+    let coll = db.collection("users")
+    checkForUser(coll, userName, userPassword).then((response) => {
+        let userExists = response;
+        if(!userExists) {
+            let doc = { userName: userName, userPassword: userPassword }
+            coll.insertOne(doc);
+        }
+
+        checkForUser(coll, userName, userPassword).then((response) => {
+            let userExists = response;
+            if(!userExists) reply.code(500).send("User POST Failed");
+            else reply.code(201).send("User POST Success");
+        })
+    })
+}
+
+async function checkForUser(coll, userName, userPassword) {
+
+    // Query for a movie that has the title 'The Room'
+    const query = { userName: userName, userPassword: userPassword };
+
+    const options = {
+        projection: { _id: 0, userName: 1, userPassword: 1 }
+    };
+      
+    const result = await coll.findOne(query, options);
+
+    if(result == null) return false;
+    return true;
 }
 
 module.exports = {
