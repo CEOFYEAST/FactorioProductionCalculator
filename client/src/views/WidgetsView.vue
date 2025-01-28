1373cf803ba2ea56d983a28bf49d75fd5da1077e
CEOFYEAST <benton.diebold@me.com>
2025-01-27 22:13:12 -0600

    Moved user data display to widgets view

--- client/src/views/WidgetsView.vue
+++ client/src/views/WidgetsView.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="AccountAccessView-root" class="root">
         <h2>User Data</h2>
-        <div v-show="submissionSuccess" style="border: solid black 2px;">{{ userData }}</div>
+        <div v-show="userStore.signedIn" style="border: solid black 2px;">{{ userStore.data }}</div>
     </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { useUserStore } from '@/stores/user';
 
-}
+let userStore = useUserStore()
 </script>
 
 <style>
