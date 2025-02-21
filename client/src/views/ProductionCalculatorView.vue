67dd8b06611a8665b5587651c7735db2c48aa59e
CEOFYEAST <benton.diebold@me.com>
2025-02-21 10:39:30 -0600

    Added preparatory file for the production calculator's view

---
+++ client/src/views/ProductionCalculatorView.vue
@@ -0,0 +1,22 @@
+<!--
+Will need a method for loading a factory into the view
+ - can be called by a lifecycle hook
+Will need to update various things whenever a factory is loaded
+ - menus will need to be refreshed
+ - local lists of items, such as a local list of input items, will need to be refreshed
+ - the actual components representing the nodes of the factory will need to be refreshed
+Will need a method for reloading a factory
+ - could just re-use the loading method, but it could lend to efficiency and understandibility if I seperate the two 
+
+-->
+
+<!--
+
+LoadFactory()
+ReloadFactory()
+
+RefreshNodes()
+RefreshMenus()
+RefreshInputs()
+
+-->
