8e65c069ea7db56084c2a92f281f721760c6e1c4
CEOFYEAST <benton.diebold@me.com>
2025-07-12 16:25:08 -0500

    Use server package

--- server/src/scripts/readConfig.js
+++
@@ -1,9 +0,0 @@
-function readConfig(jsonString) {
-    try {
-        return JSON.parse(jsonString);
-    } catch (error) {
-        throw new Error('Invalid JSON string');
-    }
-}
-
-module.exports = readConfig;
